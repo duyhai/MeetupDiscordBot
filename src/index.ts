448b75f4fe6457e94c069b63064184ba97d69713
Hai Bui <duyhai@users.noreply.github.com>
2022-03-12 15:09:28 -0800

    Add logger

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,20 @@
 import { Client } from 'discord.js';
+import { Logger } from 'tslog';
 import Configuration from './configuration';
 import interactionCreate from './listeners/interactionCreate';
 import ready from './listeners/ready';
 
 const token = Configuration.discordAPIKey;
 
-console.log('Bot is starting...');
+const logger = new Logger({ name: 'Bot' });
+logger.info('Bot is starting...');
 
 const client = new Client({
   intents: [],
 });
 
 ready(client);
+
 interactionCreate(client);
 
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
