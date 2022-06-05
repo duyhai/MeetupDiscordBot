3eb8fe0fa4d77250e03eb97fb6442fd66a2a2bef
Duy Hai Bui <duyhai@users.noreply.github.com>
2022-06-05 11:53:52 -0700

    Periodic task (#16)
    * Init project
    
    * Config
    
    * DeleteChannel command
    
    * mend
    
    * mend
    
    * Bree
    
    * Fix Bree
    
    * Remove keys
    
    * Fix PR
    
    Co-authored-by: Bui Quang Anh <bui.krisztian01@gmail.com>
    Co-authored-by: bkrisztian01 <32880536+bkrisztian01@users.noreply.github.com>

--- README.md
+++ README.md
@@ -4,6 +4,9 @@ A Discord bot for managing Meetup groups
 # How to fork repo and contribute back into the main repo
 https://www.dataschool.io/how-to-contribute-on-github/
 
+# How to set original repository as upstream
+https://docs.github.com/en/pull-requests/collaborating-with-pull-requests/working-with-forks/configuring-a-remote-for-a-fork
+
 # Development
 - We recommend using VSCode for coding
 - Once you install VSCode, use the `code-workspace` file to open the project
@@ -13,3 +16,4 @@ https://www.dataschool.io/how-to-contribute-on-github/
 - Download dependencies: `yarn`
 - Fill out the API keys in the `.env` file then run `git update-index --skip-worktree .env` to prevent checking in your keys
 - Run the script: `export $(cat .env | xargs) && yarn start`. If you have Heroku CLI (which we use to deploy the bot), you can also just run `heroku local`
+- If you are on Windows, turn off auto CRLF with this command: `git config core.autocrlf false`
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import Bree from 'bree';
 import { Intents, Interaction, Message } from 'discord.js';
 import { Client } from 'discordx';
 import express, { Request, Response } from 'express';
@@ -7,6 +8,32 @@ import './contextMenu';
 import './commands';
 
 const logger = new Logger({ name: 'MeetupBot' });
+
+/// ////////////////////////////////////////////////////////////////
+//                           CRON JOB                            //
+/// ////////////////////////////////////////////////////////////////
+
+const bree = new Bree({
+  root: 'src',
+  jobs: [
+    // {
+    //   name: 'job that sometimes throws errors',
+    //   path: () => console.log(':)'),
+    //   interval: 'one second',
+    // },
+  ],
+  errorHandler: (error, workerMetadata: Record<string, unknown>) => {
+    logger.error(
+      `There was an error while running a worker ${JSON.stringify(
+        workerMetadata
+      )}`
+    );
+
+    logger.error(error);
+  },
+});
+
+bree.start();
 
 /// ////////////////////////////////////////////////////////////////
 //                         EXPRESS SERVER                        //
