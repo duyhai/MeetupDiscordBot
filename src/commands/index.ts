e5f47a29f0d4b1b78f13f16743edea849d5c26a1
Duy Hai Bui <duyhai@users.noreply.github.com>
2023-03-26 19:04:13 -0700

    Activate MeetupGetUserRolesCommands command

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,12 +1,14 @@
 import { CreateChannel } from './createChannel';
 import { DeleteChannel } from './deleteChannel';
 import { MeetupGetEventStatsCommands } from './meetup/getEventStats';
+import { MeetupGetUserRolesCommands } from './meetup/getUserRoles';
 import { MeetupSelfOnboardCommands } from './meetup/selfOnboard';
 import { OnboardUserCommands } from './onboardUser';
 
 const Commands = [
   MeetupSelfOnboardCommands,
   MeetupGetEventStatsCommands,
+  MeetupGetUserRolesCommands,
   OnboardUserCommands,
   CreateChannel,
   DeleteChannel,
