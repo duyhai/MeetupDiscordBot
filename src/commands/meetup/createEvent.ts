5ff191f83582650b523bc5952f304144c7bfadd1
Hai Bui <bdhai91@gmail.com>
2023-04-19 17:36:24 -0400

    Set original buttons disabled in meetupRequestEventHandler

--- src/commands/meetup/createEvent.ts
+++ src/commands/meetup/createEvent.ts
@@ -16,13 +16,38 @@ const logger = new Logger({ name: 'MeetupCreateEventCommands' });
 
 @Discord()
 export class MeetupCreateEventCommands {
+  getRequestEventButtons() {
+    const approveButton = new ButtonBuilder()
+      .setLabel('Approve')
+      .setStyle(ButtonStyle.Success)
+      .setCustomId('approve');
+
+    const denyButton = new ButtonBuilder()
+      .setLabel('Deny')
+      .setStyle(ButtonStyle.Danger)
+      .setCustomId('deny');
+
+    const buttonRow =
+      new ActionRowBuilder<MessageActionRowComponentBuilder>().addComponents(
+        approveButton,
+        denyButton
+      );
+    return buttonRow;
+  }
+
   @ButtonComponent({ id: 'approve' })
   async meetupRequestApproveEventHandler(interaction: ButtonInteraction) {
+    const newButtons = this.getRequestEventButtons();
+    newButtons.components.forEach((btn) => btn.setDisabled(true));
+    await interaction.message.edit({ components: [newButtons] });
     await interaction.reply({ ephemeral: true, content: ':+1:' });
   }
 
   @ButtonComponent({ id: 'deny' })
   async meetupRequestDenyEventHandler(interaction: ButtonInteraction) {
+    const newButtons = this.getRequestEventButtons();
+    newButtons.components.forEach((btn) => btn.setDisabled(true));
+    await interaction.message.edit({ components: [newButtons] });
     await interaction.reply({ ephemeral: true, content: ':-1:' });
   }
 
@@ -35,24 +60,8 @@ export class MeetupCreateEventCommands {
       await withMeetupClient(interaction, async (_meetupClient) => {
         logger.info(`User requested event: ${interaction.user.username}`);
 
-        const approveButton = new ButtonBuilder()
-          .setLabel('Approve')
-          .setStyle(ButtonStyle.Success)
-          .setCustomId('approve');
-
-        const denyButton = new ButtonBuilder()
-          .setLabel('Deny')
-          .setStyle(ButtonStyle.Danger)
-          .setCustomId('deny');
-
-        const buttonRow =
-          new ActionRowBuilder<MessageActionRowComponentBuilder>().addComponents(
-            approveButton,
-            denyButton
-          );
-
         await interaction.editReply({
-          components: [buttonRow],
+          components: [this.getRequestEventButtons()],
         });
       });
     });
