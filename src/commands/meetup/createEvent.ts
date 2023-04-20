5d32bf55bbe5b8dc62070bf76da91cf822fb1e2b
Hai Bui <bdhai91@gmail.com>
2023-04-19 21:26:37 -0400

    Add parameters for MeetupCreateEventCommands

--- src/commands/meetup/createEvent.ts
+++ src/commands/meetup/createEvent.ts
@@ -1,18 +1,29 @@
 import {
   ActionRowBuilder,
+  ApplicationCommandOptionType,
   ButtonBuilder,
   ButtonInteraction,
   ButtonStyle,
   CommandInteraction,
   MessageActionRowComponentBuilder,
 } from 'discord.js';
-import { ButtonComponent, Discord, Slash } from 'discordx';
+import { ButtonComponent, Discord, Slash, SlashOption } from 'discordx';
 import { Logger } from 'tslog';
 
 import { discordCommandWrapper } from '../../util/discord';
 import { withMeetupClient } from '../../util/meetup';
+import { dateChecker, stringToDate } from '../../util/strings';
 
 const logger = new Logger({ name: 'MeetupCreateEventCommands' });
+
+const TITLE_MAX_LENGTH = 80;
+const GUEST_HOST_PREFIX = '[Guest Host] ';
+
+interface EventRequest {
+  eventDate: Date;
+  eventTitle: string;
+  meetupUserId: string;
+}
 
 @Discord()
 export class MeetupCreateEventCommands {
@@ -37,38 +48,105 @@ export class MeetupCreateEventCommands {
 
   @ButtonComponent({ id: 'approve' })
   async meetupRequestApproveEventHandler(interaction: ButtonInteraction) {
-    const message = await interaction.message.fetch();
-    const newButtons = this.getRequestEventButtons();
-    newButtons.components.forEach((btn) => btn.setDisabled(true));
-    await message.edit({
-      content: interaction.message.content,
-      components: [newButtons],
+    await discordCommandWrapper(interaction, async () => {
+      await withMeetupClient(interaction, async (meetupClient) => {
+        const membershipInfo = await meetupClient.getUserMembershipInfo();
+        if (!membershipInfo.groupByUrlname.isOrganizer) {
+          throw new Error(
+            `You don't have permission to do this, silly. You're not an organizer`
+          );
+        }
+
+        const message = await interaction.message.fetch();
+        const newButtons = this.getRequestEventButtons();
+        newButtons.components.forEach((btn) => btn.setDisabled(true));
+        await message.edit({
+          content: `${interaction.message.content}\nApproved request.`,
+          components: [newButtons],
+        });
+
+        await interaction.editReply(
+          'Successfully connected to Meetup. You can dismiss this message.'
+        );
+      });
     });
-    await interaction.reply({ ephemeral: true, content: ':+1:' });
   }
 
   @ButtonComponent({ id: 'deny' })
   async meetupRequestDenyEventHandler(interaction: ButtonInteraction) {
-    const newButtons = this.getRequestEventButtons();
-    newButtons.components.forEach((btn) => btn.setDisabled(true));
-    await interaction.message.edit({
-      content: interaction.message.content,
-      components: [newButtons],
+    await discordCommandWrapper(interaction, async () => {
+      await withMeetupClient(interaction, async (meetupClient) => {
+        const membershipInfo = await meetupClient.getUserMembershipInfo();
+        if (!membershipInfo.groupByUrlname.isOrganizer) {
+          throw new Error(
+            `You don't have permission to do this, silly. You're not an organizer`
+          );
+        }
+
+        const message = await interaction.message.fetch();
+        const newButtons = this.getRequestEventButtons();
+        newButtons.components.forEach((btn) => btn.setDisabled(true));
+        await message.edit({
+          content: `${interaction.message.content}\nDenied request.`,
+          components: [newButtons],
+        });
+        await interaction.reply({ ephemeral: true, content: ':-1:' });
+      });
     });
-    await interaction.reply({ ephemeral: true, content: ':-1:' });
   }
 
   @Slash({
     name: 'meetup_request_event',
     description: `Requesting the creation of a Meetup event from the Leadership Team. Output is public.`,
   })
-  async meetupRequestEventHandler(interaction: CommandInteraction) {
+  async meetupRequestEventHandler(
+    @SlashOption({
+      name: 'date',
+      description: 'Date in the format of 2023/12/01',
+      type: ApplicationCommandOptionType.String,
+      required: true,
+    })
+    date: string,
+    @SlashOption({
+      name: 'title',
+      description: 'Title for the event you want to host',
+      type: ApplicationCommandOptionType.String,
+      required: true,
+      maxLength: TITLE_MAX_LENGTH - GUEST_HOST_PREFIX.length,
+    })
+    title: string,
+    interaction: CommandInteraction
+  ) {
     await discordCommandWrapper(interaction, async () => {
-      await withMeetupClient(interaction, async (_meetupClient) => {
+      if (!dateChecker(date)) {
+        throw new Error(
+          'Invalid date. Please make sure your date has the format of YYYY/MM/DD and has valid values before trying again.'
+        );
+      }
+      if (title.includes('\n')) {
+        throw new Error(
+          'Invalid title. Please make sure it has no new line characters before trying again.'
+        );
+      }
+
+      await withMeetupClient(interaction, async (meetupClient) => {
         logger.info(`User requested event: ${interaction.user.username}`);
 
+        const userInfo = await meetupClient.getUserInfo();
+        const eventRequestInfo: EventRequest = {
+          meetupUserId: userInfo.self.id,
+          eventDate: stringToDate(date),
+          eventTitle: title,
+        };
+
+        await interaction.editReply(
+          'Successfully connected to Meetup. You can dismiss this message.'
+        );
         await interaction.followUp({
-          content: `${interaction.user.username} is requesting the creation of a new Meetup event`,
+          content: `${interaction.user.toString()} is requesting the creation of a new Meetup event`,
+        });
+        await interaction.followUp({
+          content: JSON.stringify(eventRequestInfo),
           components: [this.getRequestEventButtons()],
         });
       });
--- src/util/strings.ts
+++ src/util/strings.ts
@@ -1,3 +1,33 @@
 export function capitalize(str: string) {
   return str.charAt(0).toUpperCase() + str.slice(1);
 }
+
+export function stringToDate(dateStr: string): Date | undefined {
+  // Create a regular expression to match the YYYY/MM/DD format
+  const regex = /^\d{4}\/\d{2}\/\d{2}$/;
+
+  // Test if the string matches the regular expression
+  if (!regex.test(dateStr)) {
+    return undefined;
+  }
+
+  // Split the string into year, month, and day components
+  const [yearStr, monthStr, dayStr] = dateStr.split('/');
+  const year = parseInt(yearStr, 10);
+  const month = parseInt(monthStr, 10);
+  const day = parseInt(dayStr, 10);
+
+  // Create a new date object from the year, month, and day components
+  return new Date(year, month - 1, day);
+}
+
+export function dateChecker(dateStr: string): boolean {
+  // Create a new date object from the year, month, and day components
+  const date = stringToDate(dateStr);
+  if (!date) {
+    return false;
+  }
+
+  // Use the isNaN function to check if the date is valid
+  return !Number.isNaN(date.getTime());
+}
