2515e81857e33015d7c1721b41eeacc9613f1a87
Hai Bui <bdhai91@gmail.com>
2023-04-30 20:31:02 -0700

    Turn on MeetupAnnounceEventContextCommands

--- src/contextMenu/index.ts
+++ src/contextMenu/index.ts
@@ -1,5 +1,9 @@
+import { MeetupAnnounceEventContextCommands } from './meetup/announceEvent';
 import { OnboardUserContextCommands } from './onboardUser';
 
-const ContextCommands = [OnboardUserContextCommands];
+const ContextCommands = [
+  OnboardUserContextCommands,
+  MeetupAnnounceEventContextCommands,
+];
 
 export default ContextCommands;
