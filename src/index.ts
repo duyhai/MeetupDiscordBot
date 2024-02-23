d40e9427cfa0cfcded31dfd5aeee05fbd40a818b
Duy Hai Bui <duyhai@users.noreply.github.com>
2024-02-23 15:28:12 -0800

    Add Redis cache

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ import './buttonMenu';
 import './contextMenu';
 import './commands';
 import { generateOAuthUrl } from './constants';
-import { InMemoryCache } from './lib/cache/memoryCache';
+import { ApplicationCache } from './util/cache';
 
 const logger = new Logger({ name: 'MeetupBot' });
 
@@ -37,13 +37,19 @@ app
 app.get('/persistToken/:name/:maskedUserId', (req, res) => {
   const { maskedUserId, name } = req.params;
   const accessToken = req.query.access_token.toString();
-  InMemoryCache.instance()
-    .get(`maskedUserId-${maskedUserId}`)
-    .then((userId) =>
-      InMemoryCache.instance().set(`${userId}-${name}-accessToken`, accessToken)
-    )
-    .then(() => res.end(`Connected to Meetup. You can close this window now!`))
-    .catch(() => res.end(`Failed to connect to Meetup! Please try again.`));
+  ApplicationCache()
+    .then((cache) => {
+      cache
+        .get(`maskedUserId-${maskedUserId}`)
+        .then((userId) =>
+          cache.set(`${userId}-${name}-accessToken`, accessToken)
+        )
+        .then(() =>
+          res.end(`Connected to Meetup. You can close this window now!`)
+        )
+        .catch(() => res.end(`Failed to connect to Meetup! Please try again.`));
+    })
+    .catch(() => res.end(`Failed to data store! Please try again.`));
 });
 
 app.get('/linked-role', (_req, res) => {
