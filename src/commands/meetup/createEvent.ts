c0ba604b88b61090b7f1c352137ead77dcd85c21
Hai Bui <duyhai@users.noreply.github.com>
2025-09-24 11:30:42 +0700

    Refactor lock

--- src/commands/meetup/createEvent.ts
+++ src/commands/meetup/createEvent.ts
@@ -15,6 +15,7 @@ import { DRAFT_EVENT_TEMPLATE_ID } from '../../constants';
 import { createEventTemplate } from '../../templates/createEventTemplate';
 import { ApplicationCache } from '../../util/cache';
 import { discordCommandWrapper } from '../../util/discord';
+import { withLock } from '../../util/lock';
 import { withMeetupClient } from '../../util/meetup';
 
 const logger = new Logger({ name: 'MeetupCreateEventCommands' });
@@ -53,105 +54,86 @@ export class MeetupCreateEventCommands {
       await withMeetupClient(interaction, async (meetupClient) => {
         const lockKey = `meetupEventCreate-${interaction.message.id}`;
         const lockId = `${interaction.user.username}`;
-        logger.info(
-          `Acquiring lock ${JSON.stringify({ key: lockKey, lockId })}`
-        );
         const cache = await ApplicationCache();
-        try {
-          // Acquire lock
-          // TODO: Refactor into lock function
-          if (!(await cache.exclusive_set(lockKey, lockId))) {
-            logger.info(
-              `Lock is already taken ${JSON.stringify({
-                key: lockKey,
-                lockId,
-              })}`
-            );
-            throw new Error(
-              'Someone is already approving this request. Please stand by and try again later.'
-            );
-          }
 
-          const membershipInfo = await meetupClient.getUserMembershipInfo();
-          if (
-            membershipInfo.groupByUrlname.membershipMetadata.status !== 'LEADER'
-          ) {
-            logger.info(
-              `User without permission tried to approve: ${interaction.user.username}`
-            );
-            throw new Error(
-              `You don't have permission to do this, silly. You're not an organizer`
-            );
-          }
-          logger.info(
-            `Permission check passed for: ${interaction.user.username}`
-          );
-
-          const [, ...requestInfo] = interaction.message.content.split('\n');
-          requestInfo.pop();
-
-          const [meetupUserId, eventDate, eventTitle] = requestInfo.map(
-            (line) => {
-              const key = line.split(': ')[0];
-              return line.slice(key.length + 2);
+        await withLock({
+          cache,
+          lockKey,
+          lockId,
+          fn: async () => {
+            const membershipInfo = await meetupClient.getUserMembershipInfo();
+            if (
+              membershipInfo.groupByUrlname.membershipMetadata.status !==
+              'LEADER'
+            ) {
+              logger.info(
+                `User without permission tried to approve: ${interaction.user.username}`
+              );
+              throw new Error(
+                `You don't have permission to do this, silly. You're not an organizer`
+              );
             }
-          );
-
-          const draftEventTemplate = await meetupClient.getEvent(
-            DRAFT_EVENT_TEMPLATE_ID
-          );
-          logger.info(`Fetched template event`);
-
-          const newEvent = await meetupClient.createEvent({
-            ...createEventTemplate,
-            description: draftEventTemplate.event.description,
-            title: `${GUEST_HOST_PREFIX}${eventTitle}`,
-            startDateTime: `${eventDate}T${DEFAULT_START_TIME}:00`,
-            eventHosts: [Number(meetupUserId)],
-          });
-          logger.info(`Event created`);
-
-          const eventId = newEvent.createEvent.event.id;
-          await meetupClient.closeEventRsvps({ eventId });
-          // Have to set this again because close RSVP wipes it out...
-          await meetupClient.editEvent({
-            eventId,
-            rsvpSettings: createEventTemplate.rsvpSettings,
-          });
-          logger.info(`Event RSVP closed`);
-
-          const message = await interaction.message.fetch();
-          const newButtons = this.getRequestEventButtons();
-          newButtons.components.forEach((btn) => btn.setDisabled(true));
-          await message.edit({
-            content: `${
-              interaction.message.content
-            }\n✅ Request approved by ${interaction.user.toString()}.\nLink to event: <${
-              newEvent.createEvent.event.eventUrl
-            }>`,
-            components: [newButtons],
-          });
-          logger.info(`Event approved. Edited message: ${message.id}`);
-
-          await interaction.followUp({
-            content: '✅ Event request approved!',
-            ephemeral: true,
-          });
-          logger.info(
-            `Event approved. Responded to user: ${interaction.user.username}`
-          );
-        } finally {
-          // Free lock
-          logger.info(
-            `Checking lock to free ${JSON.stringify({ key: lockKey, lockId })}`
-          );
-          if ((await cache.get(lockKey)) === lockId) {
             logger.info(
-              `Freeing lock ${JSON.stringify({ key: lockKey, lockId })}`
+              `Permission check passed for: ${interaction.user.username}`
             );
-            await cache.remove(lockKey);
-          }
-        }
+
+            const [, ...requestInfo] = interaction.message.content.split('\n');
+            requestInfo.pop();
+
+            const [meetupUserId, eventDate, eventTitle] = requestInfo.map(
+              (line) => {
+                const key = line.split(': ')[0];
+                return line.slice(key.length + 2);
+              }
+            );
+
+            const draftEventTemplate = await meetupClient.getEvent(
+              DRAFT_EVENT_TEMPLATE_ID
+            );
+            logger.info(`Fetched template event`);
+
+            const newEvent = await meetupClient.createEvent({
+              ...createEventTemplate,
+              description: draftEventTemplate.event.description,
+              title: `${GUEST_HOST_PREFIX}${eventTitle}`,
+              startDateTime: `${eventDate}T${DEFAULT_START_TIME}:00`,
+              eventHosts: [Number(meetupUserId)],
+            });
+            logger.info(`Event created`);
+
+            const eventId = newEvent.createEvent.event.id;
+            await meetupClient.closeEventRsvps({ eventId });
+            // Have to set this again because close RSVP wipes it out...
+            await meetupClient.editEvent({
+              eventId,
+              rsvpSettings: createEventTemplate.rsvpSettings,
+            });
+            logger.info(`Event RSVP closed`);
+
+            const message = await interaction.message.fetch();
+            const newButtons = this.getRequestEventButtons();
+            newButtons.components.forEach((btn) => btn.setDisabled(true));
+            await message.edit({
+              content: `${
+                interaction.message.content
+              }\n✅ Request approved by ${interaction.user.toString()}.\nLink to event: <${
+                newEvent.createEvent.event.eventUrl
+              }>`,
+              components: [newButtons],
+            });
+            logger.info(`Event approved. Edited message: ${message.id}`);
+
+            await interaction.followUp({
+              content: '✅ Event request approved!',
+              ephemeral: true,
+            });
+            logger.info(
+              `Event approved. Responded to user: ${interaction.user.username}`
+            );
+          },
+          errorMsg:
+            'Someone is already approving this request. Please stand by and try again later.',
+        });
       });
     });
   }
---
+++ src/util/lock.ts
@@ -0,0 +1,45 @@
+import { Logger } from 'tslog';
+import { InMemoryCache } from '../lib/cache/memoryCache';
+import { RedisCache } from '../lib/cache/redisCache';
+
+const logger = new Logger({ name: 'withLock' });
+
+/**
+ * Executes a function within an exclusive lock.
+ */
+export async function withLock({
+  cache,
+  lockKey,
+  lockId,
+  fn,
+  errorMsg,
+}: {
+  cache: RedisCache | InMemoryCache;
+  errorMsg?: string;
+  fn: () => Promise<void>;
+  lockId: string;
+  lockKey: string;
+}) {
+  logger.info(`Acquiring lock ${JSON.stringify({ key: lockKey, id: lockId })}`);
+  if (!(await cache.exclusive_set(lockKey, lockId))) {
+    logger.info(
+      `Lock is already taken ${JSON.stringify({ key: lockKey, id: lockId })}`
+    );
+    throw new Error(errorMsg);
+  }
+
+  try {
+    await fn();
+  } finally {
+    // Free lock
+    logger.info(
+      `Checking lock to free ${JSON.stringify({ key: lockKey, id: lockId })}`
+    );
+    if ((await cache.get(lockKey)) === lockId) {
+      logger.info(
+        `Freeing lock ${JSON.stringify({ key: lockKey, id: lockId })}`
+      );
+      await cache.remove(lockKey);
+    }
+  }
+}
