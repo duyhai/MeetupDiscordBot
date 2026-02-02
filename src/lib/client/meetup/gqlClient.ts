d7a91e5068fbcd0d5264a33ecd8128a2a5f6fc32
Hai Bui <duyhai@users.noreply.github.com>
2026-02-02 11:15:16 +0700

    Fix logging

--- src/lib/client/meetup/gqlClient.ts
+++ src/lib/client/meetup/gqlClient.ts
@@ -128,7 +128,7 @@ export class GqlMeetupClient {
     input: PaginationInput,
     filter?: GroupEventFilter
   ) {
-    logger.info(`Calling getGroupEvents with input: ${JSON.stringify(input)}`);
+    logger.info(`Calling getGroupEvents with input: ${JSON.stringify({ input, filter })}`);
     // Can be cached because it doesn't retrieve user specific data
     return cachedClientRequest(
       'getGroupEvents',
@@ -158,7 +158,7 @@ export class GqlMeetupClient {
     input: PaginationInput,
     filter?: RsvpFilter
   ) {
-    logger.info(`Calling getEventRsvps with input: ${JSON.stringify(input)}`);
+    logger.info(`Calling getEventRsvps with input: ${JSON.stringify({ eventId, input, filter })}`);
     // Can be cached because it doesn't retrieve user specific data
     return cachedClientRequest(
       'getEventRsvps',
